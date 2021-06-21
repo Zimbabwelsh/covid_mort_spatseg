d8001c7cdcc022fb4f492ae63d762655d3007b9c
Gareth <garethjgriffith1@gmail.com>
2021-06-21 10:50:16 +0100

    upd Readme and add final figure generation

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-covid_mort_spatseg
+This code generates the data files for analyses used in Griffith et al. 2021 (Structural Inequalities in COVID-19 mortality in England and Wales, JECH, forthcoming). 
+The preprint of the paper is available on medrxiv [here]("medrxiv.org/content/10.1101/2021.02.15.21251771v1").
+
+00_data_reshaping.R constructs the dataset reqeuired for the analyses, and contains links to the locations of the data itself. 
+
+fig_generation_revisions.R demonstrates how to take MLwiN (v3.03) extracted MCMC chain output and use them to construct the results (and supplemental results) figures. 
+
+multilevel_example.R contains the code to generate the multilevel example in Figure 1.
--- fig_generation_revisions.R
+++ fig_generation_revisions.R
@@ -3,23 +3,24 @@ library(ggplot2)
 library(tidyverse)
 library(corrplot)
 library(gridExtra)
+library(viridis)
 
-setwd("C:/Users/gg9824/Dropbox/00ESRC Fellowship/Projects/COVID19/COVID Inequalities/Final Models/Finalfinal Models")
-MCMC <- fread("1A output.csv")
+setwd("C:/Users/gg9824/Dropbox/00ESRC Fellowship/Projects/COVID19/COVID Inequalities/Final Models/Finalfinal Models/No STP")
+MCMC <- fread("TTWA 2b ENGIMD output.csv")
 
 ### For calculation
 # N of iterations in MCMC samplers
 iter <- 500000
 # N of structural levels
-levels <- 4
+levels <- 3
 # Name levels
-levnames <- c("MSOA", "LAD", "STP", "Region")
+levnames <- c("MSOA", "TTWA", "Region")
 # N of fixed coefficients
-fcoefs <- 14
+fcoefs <- 24
 # N of random coefficients
 rcoefs <- 5
 # Random Coefficient Names
-rcoefnames <- c("March", "April", "May", "June", "July")
+rcoefnames <- c("Mar", "Apr", "May", "Jun", "Jul")
 # Total coefs
 coefs <- rcoefs+fcoefs
 
@@ -31,7 +32,7 @@ size <- ((rcoefs*(rcoefs+1))/2)-rcoefs
 
 # Calc N of random estimates
 rests <- size*levels
-
+  
 # Reshape data
 df <- as.data.frame(matrix(MCMC$V1, iter, ests, byrow=TRUE))
 
@@ -53,11 +54,12 @@ trino <- function(n){
   sapply(1:n, function(x) sum(1:x))
 }
 
-variances <-  trino(rcoefs)
+basevariances <-  trino(rcoefs)
+variances <- basevariances
 
 # Repeat for all levels
 repeat{  
-  variances <- append(variances, variances+max(variances))
+  variances <- append(variances, basevariances+max(variances))
   if (length(variances)==(rcoefs*levels)){
     break
   }
@@ -71,42 +73,96 @@ rawvars <- df[,varlist[variances]]
 # Take quantiles from "quants" and apply to rawvars
 varests <- as.data.frame(apply(rawvars, 2, quantile, probs = quants))
 varests <- varests %>%
-  tibble::rownames_to_column() %>%
-  pivot_longer(-rowname) %>%
+  tibble::rownames_to_column() %>%  
+  pivot_longer(-rowname) %>% 
   pivot_wider(names_from=rowname, values_from=value)
 varests <- varests[,-1]
 # Rename Cols to something sensible
 colnames(varests) <- c("Lower", "Lower 0.05", "Median", "Upper 0.95", "Upper")
-#
+
+
+################# 
+### MRR Calcs ###
+#################
+
+
 MRRests <- exp(sqrt(2*varests)*0.6745)
 # Generate level-names variable and ensure factor to maintain order for plotting
 MRRests$level <- rev(rep(levnames, each=rcoefs))
 MRRests$level <- factor(MRRests$level, levels = levnames)
 # Generate months
-MRRests$Month <- rep(3:7, levels)
+MRRests$Month <- rep(2+(1:rcoefs), levels)
+
+### replace region MRR NAs with 1.0 
+# MRRests$Lower <- MRRests$Lower %>% replace_na(1)#
+
 MRRests$type <- "Non COVID-19"
 
 ncovMRRs <- MRRests
 
-MRRestsMod1 <- bind_rows(list(covMRRs, ncovMRRs))
 
-# Produce ggplot graphic
-areavars <- ggplot(data=MRRestsMod1, aes(x=Month,y=Median, colour=level))+
-  geom_line()+
-  ylim(1,6)+
-  geom_ribbon(aes(ymin=Lower, ymax=Upper), linetype=3, alpha=0.1) +
-  facet_grid(type~level)
+########################
+# VPC Calculations ###
+#########################
 
-areavars
+### Need to extract fixed part estimates for random coefficients
 
-ggsave("Model1MRRs.png")
+raw_fixed_ests <- df[,1:rcoefs]
 
+fixed_ests <- as.data.frame(apply(raw_fixed_ests, 2, quantile, probs = quants))
+fixed_ests <- fixed_ests %>%
+  tibble::rownames_to_column() %>%  
+  pivot_longer(-rowname) %>% 
+  pivot_wider(names_from=rowname, values_from=value)
+fixed_ests <- fixed_ests[,-1]
+colnames(fixed_ests) <- c("Lower", "Lower 0.05", "Median", "Upper 0.95", "Upper")
+fixed_ests$level <- (rcoefnames)
+fixed_slim <- fixed_ests %>% select(c("Median"))
+lv3_slim <- varests[1:rcoefs,] %>% select(c("Median"))
+lv2_slim <- varests[(rcoefs+1):(2*rcoefs),] %>% select("Median")
+lv1_slim <- varests[((2*rcoefs)+1):(3*rcoefs),] %>% select("Median")
 
+VPC_data <- cbind(fixed_slim, lv3_slim, lv2_slim, lv1_slim)
+VPC_data$level <- rcoefnames
 
-### Producing and plotting correlations 
+VPC_colnames <- c("expected", rev(levnames), "month")
+colnames(VPC_data) <- VPC_colnames
+
+#Construct Marginal expectation (in average context)
+VPC_data$mu_1 <- exp(VPC_data$expected + (VPC_data$Region/2) + (VPC_data$TTWA/2) + (VPC_data$MSOA/2))
+
+#Construct Level-3 component of total variance
+VPC_data$lv3var <- ((VPC_data$mu_1)^2)*(exp(VPC_data$Region)-1)
+
+#Construct Level-2 component of total variance
+VPC_data$lv2var <- ((VPC_data$mu_1)^2)*(exp(VPC_data$Region))*(exp(VPC_data$TTWA)-1)
+
+#Construct level-1 component of total variance
+VPC_data$lv1var <- (VPC_data$mu_1)+((VPC_data$mu_1)^2)*exp(VPC_data$Region + VPC_data$TTWA)*(exp(VPC_data$MSOA)-1)
+
+# Relative contribution of Level 3 variance 
+VPC_data$VPC3 <- (VPC_data$lv3var)/(VPC_data$lv3var+VPC_data$lv2var+VPC_data$lv1var)
+
+#Relative contribution of Level 2 variance
+VPC_data$VPC2 <- (VPC_data$lv2var)/(VPC_data$lv3var+VPC_data$lv2var+VPC_data$lv1var)
+
+# Relative contribution of Level 1 variance
+VPC_data$VPC1 <- (VPC_data$lv1var)/(VPC_data$lv3var+VPC_data$lv2var+VPC_data$lv1var)
+
+VPC_data$totalvar <- (VPC_data$mu_1)+(((VPC_data$mu_1)^2)*(exp(VPC_data$Region + VPC_data$TTWA + VPC_data$MSOA)-1))
+
+# write.csv(VPC_data, "VPC_data.csv")
+
+MRRests$VPC <- c(VPC_data$VPC3, VPC_data$VPC2, VPC_data$VPC1)
+
+ncovVPCs <- MRRests
+
+#########################
+### Correlation Calcs ###
+#########################
+
 covarlist <- varlist[-variances]
 rawcovars <- df[covarlist]
-
 
 # Create first list to index first variances for correlation calculation
 # Function to create repeating ascending list (1,1,2,1,2,3...)
@@ -146,8 +202,6 @@ repeat{
 
 covarseq2 <- covarseq2+coefs
 
-##########
-# Combine lists to calculate correlations for each covariance estimate and store in rawcorrs
 
 rawcorrs <- as.data.frame(matrix(0, iter, rests))
 
@@ -171,21 +225,62 @@ corrests$level <- rev(rep(levnames, each=size))
 corrests$level <- factor(corrests$level, levels = levnames)
 
 
-corrests$month1 <- factor(rep(c("March","March","April","March","April","May","March","April","May","June"),4), 
-                      levels = c("March", "April", "May", "June", "July"))
-corrests$month2 <- factor(rep(c("April","May","May","June","June","June","July","July","July","July"),4),
-                      levels = c("March", "April", "May", "June", "July"))
+corrests$month1 <- factor(rep(c("March","March","April","March","April","May","March","April","May","June"),levels), 
+                          levels = c("March", "April", "May", "June", "July"))
+corrests$month2 <- factor(rep(c("April","May","May","June","June","June","July","July","July","July"),levels),
+                          levels = c("March", "April", "May", "June", "July"))
 ### crreate column for type of death
-corrests$type <- "COVID-19"
+corrests$type <- "Non COVID-19"
 
 # Save out correlation estimates for COVID/NonCOVID
-covcorrs <- corrests
-write.csv(covcorrs, "2covcorrs.csv")
+ncovcorrs <- corrests
+# write.csv(ncovcorrs, "TTWA 2borrs.csv")
 
+
+##########################
+### Combined MRR plots ###
+##########################
+
+MRRestsMod1 <- bind_rows(list(covMRRs, ncovMRRs))
+
+# Produce ggplot graphic
+areavars <- ggplot(data=MRRestsMod1, aes(x=Month,y=Median, colour=level))+
+  geom_line()+
+  scale_color_manual(values=c("#1b9e77","#d95f02", "#7570b3"))+
+  ylim(1,8)+
+  geom_ribbon(aes(ymin=Lower, ymax=Upper), linetype=3, alpha=0.1) +
+  facet_grid(type~level)
+
+areavars
+
+ggsave("Mod2ENGIMDMRRs.tiff")
+
+
+##########################
+### Combined VPC plots ###
+##########################
+
+VPCests <- bind_rows(list(covVPCs, ncovVPCs))
+
+VPCplot <- ggplot(data=VPCests, aes(x=Month, y=VPC, colour=level)) +
+  geom_line(size=1)+
+  ylim(0,1) +
+  ylab("Variance Partitioning Coefficient") +
+  scale_x_continuous(labels=c("3" = "Mar", "4" = "Apr", "5" = "May",
+                              "6" = "Jun", "7" = "Jul")) +
+  facet_grid(type~level)
+
+VPCplot
+
+ggsave("Mod2ENGIMDVPCplot.tiff")
+
+
+###########################
+### Combined Corr plots ###
+###########################
 
 ### combine covid and other deaths together
 correstsMod2 <-  bind_rows(list(covcorrs, ncovcorrs))
-
 
 ### make the plot
 ggplot(data = correstsMod2,
@@ -213,4 +308,8 @@ ggplot(data = correstsMod2,
   ### create 2 by 4 plot with each level as row starting with region (fct_rev)
   facet_grid(fct_rev(level)~type) 
 
-ggsave("Model1MortCorrs.png")
+ggsave("Mod2ENGIMDMortCorrs.tiff")
+ 
+
+
+
